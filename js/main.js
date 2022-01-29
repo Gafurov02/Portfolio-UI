7ef5ad57ff23c4041edfcc889ec76214e3e2ec5b
Gafurov02 <85359646+Gafurov02@users.noreply.github.com>
2022-01-29 12:35:44 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+
+function openBlock(evt, blockName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" experience-btn-active", "");
+    }
+    document.getElementById(blockName).style.display = "block";
+    evt.currentTarget.className += " experience-btn-active";
+}
+document.getElementById("defaultOpen").click();
